9eeb6e291dbaa76c262859e2802569b1826703b3
Sam Joseph <tansaku@gmail.com>
2014-05-14 14:08:04 +0100

    hash

--- directory.rb
+++ directory.rb
@@ -1,24 +1,24 @@
 student_count = 9
 #first we print the list of students
 students = [
-  "Dr. Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Michael Corleone",
-  "Alex De Large",
-  "The Alien",
-  "Terminator",
-  "Freddy Kruger",
-  "The Joker"
+  {:name => "Dr. Hannibal Lecter", :cohort => :november},
+  {:name => "Darth Vader", :cohort => :november},
+  {:name => "Nurse Ratched", :cohort => :november},
+  {:name => "Michael Corleone", :cohort => :november},
+  {:name => "Alex De Large", :cohort => :november},
+  {:name => "The Alien", :cohort => :november},
+  {:name => "Terminator", :cohort => :november},
+  {:name => "Freddy Kruger", :cohort => :november},
+  {:name => "The Joker", :cohort => :november}
 ]
 def print_header
   puts "The students of my cohort at Makers Academy"
   puts "-------------"
 end
 
-def print(names)
-  names.each do |name|
-    puts name
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
